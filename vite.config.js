66de5f5eafa80555e7935078e94333451318027f
Fernando Gutierrez <rotfok64@gmail.com>
2026-05-15 12:19:26 -0600

    Agregando superpoderes de PWA

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,32 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      // Esto permite probar la PWA mientras desarrollas (npm run dev)
+      devOptions: {
+        enabled: true 
+      },
+      manifest: {
+        name: 'AI Fitness Coach Pro',
+        short_name: 'FitnessPro',
+        description: 'Tu entrenador personal inteligente',
+        theme_color: '#a3e635', // El color verde lima para la barra del celular
+        background_color: '#09090b', // El fondo oscuro
+        display: 'standalone', // Esto oculta la barra del navegador de internet
+        icons: [
+          {
+            src: '/logo.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          }
+        ]
+      }
+    })
+  ]
 })
